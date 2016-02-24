14fa1eaca9aebe9c60d3bbf0d15c7fbfcfb99b94
Keith Gabryelski <keith@gabryelski.com>
2016-02-24 00:24:42 -0500

    better configuration

--- js/views/configuration.js
+++ js/views/configuration.js
@@ -1,27 +1,35 @@
 app.views.configuration = Backbone.View.extend({
   template: _.template($("#tpl-configuration").html()),
   events: {
-    'submit': 'play'
+    'submit': 'play',
+    'change #background_selector': 'changeBackground',
+    'change #layout_selector': 'changeLayout',
+  },
+  changeBackground: function(e) {
+    $("#background_img")[0].src = app.backgrounds.findWhere({ID: parseInt(e.target.value)}).get('image_url')
+  },
+  changeLayout: function(e) {
+    $("#layout_img")[0].src = app.boardLayouts.findWhere({ID: parseInt(e.target.value)}).get('image_url')
   },
   initialize: function() {
     this.render();
   },
   render: function(index) {
     this.$el.html(this.template(this.model.toJSON()));
-    $("#audio_volume").slider();
+    $("#audio_volume_slider").slider();
     this.delegateEvents();
     return this;
   },
   play: function(e) {
     e.preventDefault();
-    var board_layout_id = parseInt(this.$('#layout').val());
+    var board_layout_id = parseInt(this.$('#layout_selector').val());
     this.model.set({ board_layout_id: board_layout_id})
-    var background_id = parseInt(this.$('#background').val());
+    var background_id = parseInt(this.$('#background_selector').val());
     this.model.set({ background_id: background_id})
-    var tile_renderer_id = parseInt(this.$('#tile_renderer').val());
+    var tile_renderer_id = parseInt(this.$('#tile_renderer_selector').val());
     this.model.set({ tile_renderer_id: tile_renderer_id})
 
-    var audio_volume = parseInt(this.$('#audio_volume').data('slider').getValue()) / 100.0;
+    var audio_volume = parseInt(this.$('#audio_volume_slider').data('slider').getValue()) / 100.0;
     this.model.set({ audio_volume: audio_volume})
 
     var the_cookie = new Cookie({id: 'mahjong'});
