99e74e6054a785e7dcfe88e2b00bee8904cf4ab6
Keith Gabryelski <keith@gabryelski.com>
2016-02-24 00:41:10 -0500

    tile set gallery is more interesting

--- js/views/tile_set_gallery.js
+++ js/views/tile_set_gallery.js
@@ -1,13 +1,13 @@
 app.views.tile_set_gallery = Backbone.View.extend({
   template: _.template($("#tpl-tile-set-gallery").html()),
   events: {
-    "change #tile_renderer_id": "rendererSelected",
+    "change #tile_renderer_selector": "rendererSelected",
     'click #backButton': 'back',
   },
   initialize: function() {
     this.tiles = app.tiles
+    this.plug_id = 1;
     this.plug = new app.models.UnicodeTilePlug();
-    this.plug = new app.models.EncCssSpriteTilePlug();
     this.plug.tileDimensions = {
       tileWidth: 48,
       tileHeight: 64,
@@ -27,6 +27,9 @@ app.views.tile_set_gallery = Backbone.View.extend({
   },
   rendererSelected: function(e) {
     e.preventDefault();
+    this.plug_id = parseInt(e.target.value);
+    var plug_class = app.tileRenderers.findWhere({ID: this.plug_id}).get('rendering_plug_class')
+    this.plug = new plug_class()
     this.render();
   },
   back: function(e) {
